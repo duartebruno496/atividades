914bbb8dd96e4444992f3b000d0be26a3c2ffd9a
duartebruno496 <duarte.bruno496@gmail.com>
2020-06-16 12:16:45 -0300

    atividades lista 3

---
+++ Lista_3/1/bip.js
@@ -0,0 +1,23 @@
+// Faça um programa que recebe um número de entrada e repita a mensagem “BIP BIP” de acordo com o número inserido. EX: usuário inseriu 3 O programa imprime:  BIP BIP 
+//BIP BIP  
+//BIP BIP 
+
+var rs = require('readline-sync');
+var n = rs.questionInt('Digite um numero: ')
+var compara = 0;
+
+while (n > compara){
+    
+    console.log('bip bip');
+    compara++;
+    
+}
+
+
+
+
+
+
+//Logica 
+//  Programa recebe um número de entrada
+//  Programa imprime bip bip de acordo com o número inserido
